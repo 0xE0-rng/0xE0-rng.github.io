207d7d3fc2d93b7aa3713da06f676f328359fc82
0xE0-rng <0xE0-rng@users.noreply.github.com>
2025-07-15 20:00:00 +0000

    deploy: acf1b0e48b593d1f57f7f82c336a3922659b8698

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "Bruteforcing Android Patterns Using HID Devices",
+        "excerpt":"Unfortunately, my aunt died recently and left behind a perfectly good Samsung Galaxy S7 FE tablet. Unfortunately (or fortunately for you, the interested reader), we did not know the password for the device, which left us with one option: Factory Reset. Sadly, all modern Android devices employ a feature Google...","categories": [],
+        "tags": [],
+        "url": "/hid_bruteforcing/",
+        "teaser": null
+      }]
